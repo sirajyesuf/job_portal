d4332538ad10097976ad7c40ed90c5ad2b847e10
Siraj Yesuf <sirajyesuf762@gmail.com>
2022-09-09 07:16:06 +0300

    =config app typo resolved

--- config/auth.php
+++ config/auth.php
@@ -69,7 +69,7 @@ return [
             'model' => App\Models\User::class,
         ],
 
-        'admin' => [
+        'admins' => [
             'driver' => 'eloquent',
             'model' => App\Models\Admin::class,
         ],
