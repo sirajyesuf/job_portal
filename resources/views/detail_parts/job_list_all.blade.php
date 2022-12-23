4a215de12c94962af4f08eceb0d5d198053c8b70
seid_mengistu <seidmengistu88@gmail.com>
2022-12-23 23:06:10 +0300

    Landing page partially finished

---
+++ resources/views/detail_parts/job_list_all.blade.php
@@ -0,0 +1,369 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <!-- Basic Page Needs
+================================================== -->
+    <title>Job Portal</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+================================================== -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/colors/blue.css">
+
+</head>
+
+<body>
+
+    <!-- Wrapper -->
+    <div id="wrapper" class="mm-page mm-slideout" style="padding-top: 76px;">
+
+        <!-- Header Container
+================================================== -->
+        <header id="header-container" class="fullwidth" style="position: fixed;">
+
+            <!-- Header -->
+            <div id="header">
+                <div class="container">
+
+                    <!-- Left Side Content -->
+                    <div class="left-side">
+
+                        <!-- Logo -->
+                        <div id="logo">
+                            <a href="index-2.html"><img src="images/logo2.png" data-sticky-logo="images/logo.png"
+                                    data-transparent-logo="images/logo2.png" alt=""></a>
+                        </div>
+
+                        <!-- Main Navigation -->
+
+                        <div class="clearfix"></div>
+                        <!-- Main Navigation / End -->
+
+                    </div>
+                    <!-- Left Side Content / End -->
+
+
+                    <!-- Right Side Content / End -->
+                    <div class="right-side">
+                        <div class="header-widget">
+                            <a href="#sign-in-dialog" class="popup-with-zoom-anim log-in-button"><i
+                                    class="icon-feather-log-in"></i> <span>Log In / Register</span></a>
+                        </div>
+
+
+                        <!-- Mobile Navigation Button -->
+                        <span class="mmenu-trigger">
+                            <button class="hamburger hamburger--collapse" type="button">
+                                <span class="hamburger-box">
+                                    <span class="hamburger-inner"></span>
+                                </span>
+                            </button>
+                        </span>
+
+                    </div>
+                    <!-- Right Side Content / End -->
+
+                </div>
+            </div>
+            <!-- Header / End -->
+
+        </header>
+        <div class="clearfix"></div>
+        <!-- Header Container / End -->
+        <div class="margin-top-90"></div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-3 col-lg-4">
+                    <div class="sidebar-container">
+
+
+
+                        <!-- Job Category -->
+                        <div class="sidebar-widget">
+                            <h3>Job Category</h3>
+                            <select class="selectpicker default" multiple data-selected-text-format="count"
+                                data-size="7" title="Job Categories">
+                                <option>Admin Support</option>
+                                <option>Customer Service</option>
+                                <option>Data Analytics</option>
+                                <option>Design & Creative</option>
+                                <option>Legal</option>
+                                <option>Software Developing</option>
+                                <option>IT & Networking</option>
+                                <option>Writing</option>
+                                <option>Translation</option>
+                                <option>Sales & Marketing</option>
+                            </select>
+                        </div>
+                        <!-- Region-->
+                        <div class="sidebar-widget">
+                            <h3>Region</h3>
+                            <select class="selectpicker default" multiple data-selected-text-format="count"
+                                data-size="7" title="Region">
+                                <option>Tigray</option>
+                                <option>Afar</option>
+                                <option>Amhara</option>
+                                <option>Oromiya</option>
+
+                            </select>
+                        </div>
+                        <!-- Employment Type -->
+                        <div class="sidebar-widget">
+                            <h3>Employment Type</h3>
+                            <select class="selectpicker default" multiple data-selected-text-format="count"
+                                data-size="7" title="Employment Type">
+                                <option>Full Time</option>
+                                <option>Part Time</option>
+                            </select>
+                        </div>
+                        <!-- Carrer Level -->
+                        <div class="sidebar-widget">
+                            <h3>Carrer Level</h3>
+                            <select class="selectpicker default" multiple data-selected-text-format="count"
+                                data-size="7" title="Carrer Level">
+                                <option>Full Time</option>
+                                <option>Part Time</option>
+                            </select>
+                        </div>
+
+
+
+                    </div>
+                </div>
+                <div class="col-xl-9 col-lg-8 content-left-offset">
+
+                    <h3 class="page-title">Search Results</h3>
+
+                    <div class="notify-box margin-top-15">
+
+
+                        <div class="sort-by">
+                            <span>Sort by:</span>
+                            <select class="selectpicker hide-tick">
+                                <option>Relevance</option>
+                                <option>Newest</option>
+                                <option>Oldest</option>
+                                <option>Random</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="listings-container margin-top-35">
+
+                        <!-- Job Listing -->
+                        <a href="/single_job_detail" class="job-listing">
+
+                            <!-- Job Listing Details -->
+                            <div class="job-listing-details">
+                                <!-- Logo -->
+                                <div class="job-listing-company-logo">
+                                    <img src="images/company-logo-01.png" alt="">
+                                </div>
+
+                                <!-- Details -->
+                                <div class="job-listing-description">
+                                    <h4 class="job-listing-company">Hexagon <span class="verified-badge"
+                                            title="Verified Employer" data-tippy-placement="top"></span></h4>
+                                    <h3 class="job-listing-title">Bilingual Event Support Specialist</h3>
+                                    <p class="job-listing-text">Leverage agile frameworks to provide a robust synopsis
+                                        for high level overviews. Iterative approaches to corporate strategy foster
+                                        collaborative thinking to further the overall value.</p>
+                                </div>
+
+                                <!-- Bookmark -->
+                                <span class="bookmark-icon"></span>
+                            </div>
+
+                            <!-- Job Listing Footer -->
+                            <div class="job-listing-footer">
+                                <ul>
+                                    <li><i class="icon-material-outline-location-on"></i> Dessie</li>
+                                    <li><i class="icon-material-outline-business-center"></i> Full Time</li>
+                                    <li><i class="icon-material-outline-account-balance-wallet"></i> $35000-$38000</li>
+                                    <li><i class="icon-material-outline-access-time"></i> 2 days ago</li>
+                                </ul>
+                            </div>
+                        </a>
+
+                        <!-- Job Listing -->
+                        <a href="/single_job_detail" class="job-listing">
+
+                            <!-- Job Listing Details -->
+                            <div class="job-listing-details">
+                                <!-- Logo -->
+                                <div class="job-listing-company-logo">
+                                    <img src="images/company-logo-02.png" alt="">
+                                </div>
+
+                                <!-- Details -->
+                                <div class="job-listing-description">
+                                    <h4 class="job-listing-company">Coffee</h4>
+                                    <h3 class="job-listing-title">Barista and Cashier</h3>
+                                    <p class="job-listing-text">Leverage agile frameworks to provide a robust synopsis
+                                        for high level overviews. Iterative approaches to corporate strategy foster
+                                        collaborative thinking to further the overall value.</p>
+                                </div>
+
+                                <!-- Bookmark -->
+                                <span class="bookmark-icon"></span>
+                            </div>
+
+                            <!-- Job Listing Footer -->
+                            <div class="job-listing-footer">
+                                <ul>
+                                    <li><i class="icon-material-outline-location-on"></i> Dessie</li>
+                                    <li><i class="icon-material-outline-business-center"></i> Full Time</li>
+                                    <li><i class="icon-material-outline-account-balance-wallet"></i> $35000-$38000</li>
+                                    <li><i class="icon-material-outline-access-time"></i> 2 days ago</li>
+                                </ul>
+                            </div>
+                        </a>
+
+
+                        <!-- Job Listing -->
+                        <a href="/single_job_detail" class="job-listing">
+
+                            <!-- Job Listing Details -->
+                            <div class="job-listing-details">
+                                <!-- Logo -->
+                                <div class="job-listing-company-logo">
+                                    <img src="images/company-logo-03.png" alt="">
+                                </div>
+
+                                <!-- Details -->
+                                <div class="job-listing-description">
+                                    <h4 class="job-listing-company">King <span class="verified-badge"
+                                            title="Verified Employer" data-tippy-placement="top"></span></h4>
+                                    <h3 class="job-listing-title">Restaurant General Manager</h3>
+                                    <p class="job-listing-text">Leverage agile frameworks to provide a robust synopsis
+                                        for high level overviews. Iterative approaches to corporate strategy foster
+                                        collaborative thinking to further the overall value.</p>
+                                </div>
+
+                                <!-- Bookmark -->
+                                <span class="bookmark-icon"></span>
+                            </div>
+
+                            <!-- Job Listing Footer -->
+                            <div class="job-listing-footer">
+                                <ul>
+                                    <li><i class="icon-material-outline-location-on"></i> Dessie</li>
+                                    <li><i class="icon-material-outline-business-center"></i> Full Time</li>
+                                    <li><i class="icon-material-outline-account-balance-wallet"></i> $35000-$38000</li>
+                                    <li><i class="icon-material-outline-access-time"></i> 2 days ago</li>
+                                </ul>
+                            </div>
+                        </a>
+
+                        <!-- Job Listing -->
+                        <a href="/single_job_detail" class="job-listing">
+
+                            <!-- Job Listing Details -->
+                            <div class="job-listing-details">
+                                <!-- Logo -->
+                                <div class="job-listing-company-logo">
+                                    <img src="images/company-logo-04.png" alt="">
+                                </div>
+
+                                <!-- Details -->
+                                <div class="job-listing-description">
+                                    <h4 class="job-listing-company">Mates</h4>
+                                    <h3 class="job-listing-title">Administrative Assistant</h3>
+                                    <p class="job-listing-text">Leverage agile frameworks to provide a robust synopsis
+                                        for high level overviews. Iterative approaches to corporate strategy foster
+                                        collaborative thinking to further the overall value.</p>
+                                </div>
+
+                                <!-- Bookmark -->
+                                <span class="bookmark-icon"></span>
+                            </div>
+
+                            <!-- Job Listing Footer -->
+                            <div class="job-listing-footer">
+                                <ul>
+                                    <li><i class="icon-material-outline-location-on"></i> Dessie</li>
+                                    <li><i class="icon-material-outline-business-center"></i> Full Time</li>
+                                    <li><i class="icon-material-outline-account-balance-wallet"></i> $35000-$38000</li>
+                                    <li><i class="icon-material-outline-access-time"></i> 2 days ago</li>
+                                </ul>
+                            </div>
+                        </a>
+
+
+                        <!-- Pagination -->
+                        <div class="clearfix"></div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <!-- Pagination -->
+                                <div class="pagination-container margin-top-30 margin-bottom-60">
+                                    <nav class="pagination">
+                                        <ul>
+                                            <li class="pagination-arrow"><a href="#"><i
+                                                        class="icon-material-outline-keyboard-arrow-left"></i></a></li>
+                                            <li><a href="#">1</a></li>
+                                            <li><a href="#" class="current-page">2</a></li>
+                                            <li><a href="#">3</a></li>
+                                            <li><a href="#">4</a></li>
+                                            <li class="pagination-arrow"><a href="#"><i
+                                                        class="icon-material-outline-keyboard-arrow-right"></i></a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Pagination / End -->
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!-- Footer
+================================================== -->
+        @include("layouts.footer");
+
+        <!-- Footer / End -->
+
+    </div>
+
+    <!-- Scripts
+================================================== -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/jquery-migrate-3.1.0.min.html"></script>
+    <script src="js/mmenu.min.js"></script>
+    <script src="js/tippy.all.min.js"></script>
+    <script src="js/simplebar.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/bootstrap-select.min.js"></script>
+    <script src="js/snackbar.js"></script>
+    <script src="js/clipboard.min.js"></script>
+    <script src="js/counterup.min.js"></script>
+    <script src="js/magnific-popup.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/custom.js"></script>
+
+    <script>
+        // Snackbar for user status switcher
+        $('#snackbar-user-status label').click(function () {
+            Snackbar.show({
+                text: 'Your status has been changed!',
+                pos: 'bottom-center',
+                showAction: false,
+                actionText: "Dismiss",
+                duration: 3000,
+                textColor: '#fff',
+                backgroundColor: '#383838'
+            });
+        });
+
+    </script>
+</body>
+
+
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,49 @@
+<header id="header-container" class="fullwidth transparent-header">
+
+    <!-- Header -->
+    <div id="header">
+        <div class="container">
+
+            <!-- Left Side Content -->
+            <div class="left-side">
+
+                <!-- Logo -->
+                <div id="logo">
+                    <a href="index-2.html"><img src="images/logo2.png" data-sticky-logo="images/logo.png"
+                            data-transparent-logo="images/logo2.png" alt=""></a>
+                </div>
+
+                <!-- Main Navigation -->
+
+                <div class="clearfix"></div>
+                <!-- Main Navigation / End -->
+
+            </div>
+            <!-- Left Side Content / End -->
+
+
+            <!-- Right Side Content / End -->
+            <div class="right-side">
+                <div class="header-widget">
+                    <a href="#sign-in-dialog" class="popup-with-zoom-anim log-in-button"><i
+                            class="icon-feather-log-in"></i> <span>Log In / Register</span></a>
+                </div>
+
+
+                <!-- Mobile Navigation Button -->
+                <span class="mmenu-trigger">
+                    <button class="hamburger hamburger--collapse" type="button">
+                        <span class="hamburger-box">
+                            <span class="hamburger-inner"></span>
+                        </span>
+                    </button>
+                </span>
+
+            </div>
+            <!-- Right Side Content / End -->
+
+        </div>
+    </div>
+    <!-- Header / End -->
+
+</header>
